930628c742f6aa90a42a95337c7c90c01fbaa20a
LL <1059287151@qq.com>
2026-05-31 20:54:13 +0800

    fix: 用户状态改为线程获取

--- src/main/java/com/ll/blog/controller/StatusController.java
+++ src/main/java/com/ll/blog/controller/StatusController.java
@@ -3,6 +3,7 @@ package com.ll.blog.controller;
 import com.ll.blog.model.vo.StatusNowVO;
 import com.ll.blog.result.Result;
 import com.ll.blog.service.StatusService;
+import com.ll.blog.utils.UserHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,8 +20,9 @@ public class StatusController {
     private final StatusService statusService;
 
     @GetMapping("/now")
-    public Result<StatusNowVO> getStatusNow(@RequestAttribute("userId") Long userId){
+    public Result<StatusNowVO> getStatusNow(){
         log.info("获取当前状态");
+        Long userId = UserHolder.getUser().getId();
         StatusNowVO vo = statusService.getStatusNow(userId);
         return Result.success(vo);
     }
